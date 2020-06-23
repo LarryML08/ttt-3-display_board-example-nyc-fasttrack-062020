476c08339b9036ef4ef3a6d5e77e1d2626f73d37
Larry Lora <larryml08@icloud.com>
2020-06-23 00:36:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+puts board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board
+  puts "A Tic Tac Toe Board"
+  puts row = ["   ""|"""   ""|"""   "]
+  puts separator = "-----------"
+  puts row
+  puts separator
+  puts row
+
+end
+
+puts display_board
