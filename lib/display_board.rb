ad581175e4329e95e56e090c13ab5ba08a363d7c
Larry Lora <larryml08@icloud.com>
2020-06-23 00:46:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,6 @@
 puts board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def display_board
-  puts "A Tic Tac Toe Board"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
